10151afff19b634fd1efc74298d3c07f02936816
sdh <donghun.song@kt.com>
2022-01-26 17:25:27 +0900

    update analysis data

---
+++ modules/AccuracyFilter.py
@@ -0,0 +1,9 @@
+from enum import Enum
+import modules.Analysis.AnalysisToolForSTT as ast
+
+class AccuracyFilter(Enum):
+    EXP_BASED = ast.calculateSTTAccuracy
+    WER = ast.calculateWER
+
+### get all keys
+# [k for k in AccuracyFilter.__dict__.keys() if not k.startswith('_')]
