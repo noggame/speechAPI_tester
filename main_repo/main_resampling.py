95ac268644912dc4fafcba2a2c4e0fd8e4283f85
송동훈 <songdonghun@songdonghun-ui-MacBookPro.local>
2022-07-05 19:11:32 +0900

    delete main_repo dir

--- main_repo/main_resampling.py
+++
@@ -1,6 +0,0 @@
-import modules.SoundConverter as sc
-
-### up-sampling
-targetDirPath = '/mnt/d/Workspace/python/speechAPI_tester/sample/shinhan/split/1_20220111_101404438_2011.raw_Callee'
-# targetDirPath = '/mnt/d/Workspace/python/speechAPI_tester/sample/shinhan/test'
-sc.makeResamplingData(targetDirPath, 8000, 16000)
