c47eef0e6f89c8de5a0d4d76afea3bf52ff2ea3e
송동훈 <songdonghun@songdonghun-ui-MacBookPro.local>
2022-07-12 13:16:23 +0900

    update environmentes for prod.

--- main_restapi.py
+++ main_restapi.py
@@ -7,34 +7,27 @@ import logging
 from flask import Flask, request
 from modules.Controller.TestController import TestController
 from modules.Service.Type import SERVICE_TYPE
-from modules.DesignPattern.Factory import DataParserFactory as df, ServiceFactory as sf, AnlalyzerFactory as af
-
-
+from modules.DesignPattern.Factory import DataParserFactory as df, ServiceFactory as sf
 
 
 app = Flask(__name__)
 base_url = ''
+tc = TestController()
 
 @app.route(f"{base_url}/")
 def hello_world():
     return "<p>HWorld!</p>"
 
 
-@app.route(f"{base_url}/test/stt", methods=['Post'])
+@app.route(f"{base_url}/test", methods=['Post'])
 def test_accuracy():
 
     ### Get request_data
     try:
         params = json.loads(request.get_data(as_text=True))
 
-        # testdata = {
-        #     'data' : params['data_name'],
-        #     'api' : params['api_name'],
-        #     'number' : 2
-        # }
-
         test_option = {
-            'data_limit' : params['data_limit'],
+            'data_limit' : 1, # int(params['data_limit']),
             'update_data' : True
         }
 
@@ -43,18 +36,12 @@ def test_accuracy():
         service_type = params['service_type']
 
     except ValueError as ve:
-        print(ve)
+        logging.error("[ERROR] ValueError occured :: main_restapi - {}".format(ve))
+        return None
+    except:
+        logging.error("[ERROR] unexpected error occured :: main_restapi")
         return None
 
-
-    tc = TestController()
-
-    ########## Face
-    # tc = TestController()
-    test_option = {
-        'data_limit' : 1,
-        'update_data' : True
-    }
 
     try:
         testResultList = tc.testWith(testdata = df.DATA_NAME.__getitem__(name=data_name),
@@ -68,57 +55,6 @@ def test_accuracy():
     if not testResultList:
         return "empry"
     return str("샘플수 : {}, 평균_정확도 : {}".format(testResultList[0], testResultList[1]))
-
-
-
-@app.route(f"{base_url}/test/stt", methods=['Post'])
-def accuracy_stt():
-    try:
-        params = json.loads(request.get_data(as_text=True))
-    except ValueError as ve:
-        print(ve)
-        return None
-    
-    ### Input
-    testdata = {
-        'data' : params['data_name'],
-        'api' : params['api_name'],
-        'number' : 2
-    }
-
-    stc = STTTestController()
-    result = stc.startTestAndAnalysis(data_name=testdata["data"], api_name=testdata["api"], number=testdata["number"])
-    print(result)
-
-    if not result:
-        return "empty"
-
-    return str(result)
-
-
-@app.route(f"{base_url}/test/faceDetection", methods=['Post'])
-def accuracy_face():
-    try:
-        params = json.loads(request.get_data(as_text=True))
-    except ValueError as ve:
-        print(ve)
-        return None
-    
-    ### Input
-    testdata = {
-        'data' : params['data_name'],
-        'api' : params['api_name'],
-        'number' : 2
-    }
-
-    ftc = FaceTestController()
-    result = ftc.startTestAndAnalysis(data_name=testdata["data"], api_name=testdata["api"], number=testdata["number"])
-    print(result)
-
-    if not result:
-        return "empty"
-
-    return str(result)
 
 
 if __name__ == '__main__':
